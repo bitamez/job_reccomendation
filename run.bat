4ee841b8a8de6a4601a0bdfa31fc495ae61b9708
mesi0621 <mezgebemessi@gmail.com>
2026-05-25 21:10:58 -0700

    config: update run.bat with correct environment paths

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 echo Starting AI Job Recommendation System...
 
 REM Set local environment variables for the downloaded tools
-set JAVA_HOME=C:\Program Files\Java\jdk-24
-set MAVEN_HOME=C:\tools\maven\apache-maven-3.9.6
+set JAVA_HOME=C:\Users\user\Downloads\OpenJDK17U-jdk_x64_windows_hotspot_17.0.18_8\jdk-17.0.18+8
+set MAVEN_HOME=C:\Users\user\Downloads\apache-maven-3.9.12-bin\apache-maven-3.9.12
 set PATH=%MAVEN_HOME%\bin;%JAVA_HOME%\bin;%PATH%
 
 echo Compiling and running via Maven...
